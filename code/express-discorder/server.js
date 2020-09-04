3c5c484014ef8c5ac9a7b32e6c380a0f4d2e0072
Craig Dennis <cdennis@twilio.com>
2020-09-03 21:10:00 -0700

    u2v5

--- code/express-discorder/server.js
+++ code/express-discorder/server.js
@@ -18,8 +18,12 @@ app.get("/", (req, res) => res.send(`
 `));
 
 app.post("/github", (req, res) => {
-  const content = ":wave: Hi mom!";
-  const avatarUrl = "https://media.giphy.com/media/3o7TKSjRrfIPjeiVyM/giphy.gif";
+  // TODO: Change the content variable to contain the repository name and 
+  // The github user name...and emoji flair.
+  const username = req.body.sender.login;
+  const repoName = req.body.repository.name;
+  const content = `:taco: :taco: ${username} just starred ${repoName} :rocket: :tada: :tada: :taco:`;
+  const avatarUrl = req.body.sender.avatar_url;
   axios
     .post(process.env.DISCORD_WEBHOOK_URL, {
       content: content,
