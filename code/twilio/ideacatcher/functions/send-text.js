e5419664e0425ee6079c7634cc04c06a8548579d
Craig Dennis <cdennis@twilio.com>
2020-09-04 23:55:06 -0700

    Finishes send text function

--- code/twilio/ideacatcher/functions/send-text.js
+++ code/twilio/ideacatcher/functions/send-text.js
@@ -1,5 +1,15 @@
 exports.handler = (context, event, callback) => {
+  const client = context.getTwilioClient();
   console.log("Sending text...");
-  console.log(`Transcription: ${event.TranscriptionText}`);
-  callback(null, "Hi mom!");
+  client.messages.create({
+    to: context.PHONE_NUMBER,
+    from: context.TWILIO_NUMBER,
+    body: `New idea 💡: ${event.TranscriptionText}`
+  }).then((message) => {
+    console.log(`Sent message ${message.sid}`);
+    callback(null, `Sent message ${message.sid}`);
+  }).catch((error) => {
+    console.error(`Uh oh: ${error}`);
+    callback(error);
+  });
 };
